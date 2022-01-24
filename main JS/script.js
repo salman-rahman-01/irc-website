655bb6964d504afcc2ca24d9d56ec262f36b5126
salman-rahman-01 <mdsalmanrahman731@gmail.com>
2022-01-24 15:03:38 +0600

    first commit

---
+++ main JS/script.js
@@ -0,0 +1,80 @@
+// let searchBtn = document.querySelector('#search-btn');
+// let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.nav-bar');
+let imgBtn = document.querySelectorAll('.img-btn');
+
+window.onscroll = () =>{
+
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+formBtn.addEventListener('click', () =>{
+    loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () =>{
+    loginForm.classList.remove('active');
+});
+
+imgBtn.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#img-slider').src = src;
+    });
+});
+
+var swiper = new Swiper(".brand-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView:3,
+        },
+    },
+});
+
+
+// let i = 0;
+// let index = 0;
+// let slider = document.getElementsByClassName("slider");
+
+// function show(n) {
+//   for (i = 0; i < slider.length; i++) {
+//     slider[i].style.display = "none";
+//   }
+
+//   slider[n - 1].style.display = "block";
+// }
+// auto();
+
+// function auto() {
+//   index++;
+//   if (index > slider.length) {
+//     index = 1;
+//   }
+//   show(index);
+//   setTimeout(auto, 3000);
+// }
---
+++ management/script.js
@@ -0,0 +1,46 @@
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.nav-bar');
+
+window.onscroll = () =>{
+    
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+formBtn.addEventListener('click', () =>{
+    loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () =>{
+    loginForm.classList.remove('active');
+});
+
+
+var swiper = new Swiper(".management", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView:3,
+        },
+    },
+});
